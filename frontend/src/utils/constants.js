3ce2e591fe996805504107ad721cff78718a9f8b
AliRahimi <ali.1996rahimiourzgani@gmail.com>
2026-03-11 17:23:45 -0400

    changed the name of hte application from newsexplorer to newsapp

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,6 @@
 export const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://api.newsExplorer.hardsoft.nu"
+    ? "https://api.newsapp.home.kg"
     : "http://localhost:3001";
 
 export const APIkey = "c200de4185fa465ca50a0a30109fa3dc";
